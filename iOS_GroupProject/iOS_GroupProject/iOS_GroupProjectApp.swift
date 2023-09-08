d576d1d0d041f84c55b3861f051bac65ee6c74ef
Thu Nguyen <101914949+npat273@users.noreply.github.com>
2023-09-08 21:49:53 +0700

    Added iOS project

---
+++ iOS_GroupProject/iOS_GroupProject/iOS_GroupProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_GroupProjectApp.swift
+//  iOS_GroupProject
+//
+//  Created by Thu Nguyen  on 08/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct iOS_GroupProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
